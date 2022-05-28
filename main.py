362613de84245ee09c21568c31e129972d5f3e17
LizzySawyer <105354784+LizzySawyer@users.noreply.github.com>
2022-05-28 10:06:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+# Check if a word is an anagrams 
+# Example:
+# find_anagrams("hello") --> False
+# find_anagrams("racecar") --> True
+
+
+def find_anagrams(word1, word2):
+    # [assignment] Add your code here
+    word1 = input(str("first word: "))
+    word2 = input(str("second word: "))
+    if sorted(word1) == sorted(word2):
+        return True 
+    if sorted(word1) != sorted(word2):
+        return False
+
+print(find_anagrams('first word', 'second word2'))
+
+
+
+
+
+
+
+
+
+
+
+
+
